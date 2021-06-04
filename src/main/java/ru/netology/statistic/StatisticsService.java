b43a22797788ac777425ee6a753c1316b2bd7a7c
Zlodejrs <trotsergey@list.ru>
2021-06-04 23:25:02 +0300

    last

---
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -0,0 +1,66 @@
+package ru.netology.statistic;
+
+public class StatisticsService {
+    public long calculateSum(long[] purchases) {
+        long sum = 0;
+        for (long purchase : purchases) {
+            sum += purchase;
+        }
+        return sum;
+    }
+
+
+    public long findMax(long[] purchases) {
+        long currentMax = purchases[0];
+        for (long purchase : purchases) {
+            if (currentMax < purchase) {
+                currentMax = purchase;
+            }
+        }
+        return currentMax;
+    }
+
+    public long findAverage(long[] purchases) {
+        return calculateSum(purchases) / purchases.length;
+        }
+
+
+        public long findMaxIndex(long[] purchases) {
+        int maxIndex = 0;
+        for (int i = 0; i < purchases.length; i++) {
+            if (purchases [i] >= purchases[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+            return maxIndex + 1;
+        }
+
+    public long findMinIndex(long[] purchases) {
+        int minIndex = 0;
+        for (int i = 0; i < purchases.length; i++) {
+            if (purchases[i] <= purchases[minIndex]) {
+                minIndex = i;
+            }
+        }
+        return minIndex + 1;
+    }
+
+    public long belowAverage(long[] purchases) {
+        int index = 0;
+        long average = calculateSum(purchases) / purchases.length;
+        for (int i = 0; i < purchases.length; i++) {
+            if (purchases[i] < average)
+                index++;
+        }
+        return index;
+    }
+    public long aboveAverage(long[] purchases) {
+        int index = 0;
+        long average = calculateSum(purchases) / purchases.length;
+        for (int i = 0; i < purchases.length; i++) {
+            if (purchases[i] > average)
+                index++;
+        }
+        return index;
+    }
+}
---
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -0,0 +1,72 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StatisticsServiceTest {
+    @Test
+    void calculateSum() {
+        StatisticsService service = new StatisticsService();
+
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 180;
+
+        long actual = service.calculateSum(purchases);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 20;
+
+        long actual = service.findMax(purchases);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findAverage() {
+        StatisticsService service = new StatisticsService();
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 15;
+        long actual = service.findAverage(purchases);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void findMaxIndex() {
+        StatisticsService service = new StatisticsService();
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 8;
+        long actual = service.findMaxIndex(purchases);
+    }
+
+    @Test
+    void findMinIndex() {
+        StatisticsService service = new StatisticsService();
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 9;
+        long actual = service.findMinIndex(purchases);
+        assertEquals(expected, actual);
+    }
+    @Test
+    void belowAverage() {
+        StatisticsService service = new StatisticsService();
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 5;
+        long actual = service.belowAverage(purchases);
+        assertEquals(expected, actual);
+    }
+    @Test
+    void aboveAverage() {
+        StatisticsService service = new StatisticsService();
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 5;
+        long actual = service.aboveAverage(purchases);
+        assertEquals(expected, actual);
+    }
+}
